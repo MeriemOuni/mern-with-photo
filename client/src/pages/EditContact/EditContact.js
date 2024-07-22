3ca4ac32f8d3623a617bc99add8acdb119647121
meriem <elounimeriem@gmail.com>
2024-07-22 12:18:32 +0100

    second commit

--- client/src/pages/EditContact/EditContact.js
+++ client/src/pages/EditContact/EditContact.js
@@ -5,60 +5,88 @@ import { editContact, getOneContact } from "../../JS/Actions/contact";
 import { Button, TextField } from "@mui/material";
 
 const EditContact = () => {
-  
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const match = useMatch("/EditContact/:id");
-  const [newContact, setNewContact] = useState({});
+	const match = useMatch("/EditContact/:id");
+	const [newContact, setNewContact] = useState({});
 
-  const [file, setFile] = useState(null);
+	const [file, setFile] = useState(null);
 
-  const contactToGet = useSelector(
-    (state) => state.contactReducer.contactToGet
-  );
-  const load = useSelector((state) => state.contactReducer.load);
+	const contactToGet = useSelector(
+		(state) => state.contactReducer.contactToGet
+	);
+	const load = useSelector((state) => state.contactReducer.load);
 
-  const handleChange = (e) => {
-    setNewContact({ ...newContact, [e.target.value]: e.target.name });
-  };
+	const handleChange = (e) => {
+		setNewContact({ ...newContact, [e.target.value]: e.target.name });
+	};
 
-  useEffect(() => {
-    dispatch(getOneContact(match.params.id));
-  },[]);
+	useEffect(() => {
+		dispatch(getOneContact(match.params.id));
+	}, []);
 
-  const handlePhoto = (e) => {
-    setFile(e.target.files[0]);
-  };
+	const handlePhoto = (e) => {
+		setFile(e.target.files[0]);
+	};
 
-  const handleEdit = (e) => {
-    e.preventDefault();
-    let data = new FormData();
-    data.append("name", newContact.name);
-    data.append("phone", newContact.phone);
-    data.append("email", newContact.email);
-    data.append("image", file);
-    
-    dispatch(editContact(match.params.id, data));
-  };
+	const handleEdit = (e) => {
+		e.preventDefault();
+		let data = new FormData();
+		data.append("name", newContact.name);
+		data.append("phone", newContact.phone);
+		data.append("email", newContact.email);
+		data.append("image", file);
 
-  return (
-    <div>
-      <h1>Edit contact</h1>
+		dispatch(editContact(match.params.id, data));
+	};
 
-      <TextField id="standard-basic" type="text" label={`${contactToGet.name}`} variant="standard" onChange={handleChange} name="name" />
-      <br/><br/>
-      <TextField id="standard-basic" type="email" label={`${contactToGet.email}`}  variant="standard" onChange={handleChange} name="email" />
-      <br/><br/>
-      <TextField id="standard-basic" type="number" label={`${contactToGet.phone}`}  variant="standard" onChange={handleChange} name="phone" />
-      <br/><br/>
-      <input type="file" id="file-input" encType="multipart/form-data" onChange={handlePhoto} />
-      <br/><br/>
-      <Button variant="contained" color="success" onClick={handleEdit} >
-        Edit
-      </Button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Edit contact</h1>
+			<TextField
+				id='standard-basic'
+				type='text'
+				label={`${contactToGet.name}`}
+				variant='standard'
+				onChange={handleChange}
+				name='name'
+			/>
+			<br />
+			<br />
+			<TextField
+				id='standard-basic'
+				type='email'
+				label={`${contactToGet.email}`}
+				variant='standard'
+				onChange={handleChange}
+				name='email'
+			/>
+			<br />
+			<br />
+			<TextField
+				id='standard-basic'
+				type='number'
+				label={`${contactToGet.phone}`}
+				variant='standard'
+				onChange={handleChange}
+				name='phone'
+			/>
+			<br />
+			<br />
+			<input
+				type='file'
+				id='file-input'
+				encType='multipart/form-data'
+				onChange={handlePhoto}
+			/>
+			<br />
+			<br />
+			<Button variant='contained' color='success' onClick={handleEdit}>
+				Edit
+			</Button>
+		</div>
+	);
 };
 
 export default EditContact;
